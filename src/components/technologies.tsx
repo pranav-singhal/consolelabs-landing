577280ef49e7ca0e72d1b957eb314bf37979daed
Pranav Singhal <pranavsinghal96@gmail.com>
2024-10-07 14:19:04 -0400

    small text adjustment

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -1,7 +1,7 @@
 export function Technologies() {
   return (
     <section className="container flex flex-col items-center gap-10 py-24 relative">
-      <h2 className="font-heading text-3xl font-semibold sm:text-4xl">Our Live projects today</h2>
+      <h2 className="font-heading text-3xl font-semibold sm:text-4xl">Our Live solutions today</h2>
       <p className="max-w-2xl text-center text-lg text-muted-foreground">
         When you work with us, we ensure that our dashboards are custom tailored for your business
         flows
